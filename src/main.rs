e2f86cf81cbc330882edd7103855f2bf46bb29b2
alanStocco <stocco.alan@gmail.com>
2023-11-29 17:47:55 +0100

    (code): catch error if guess not a num.

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,21 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // _ is a catchall value. Continue, which tells the program to go to the next iteration of the loop and ask for another guess. 
+        };
     
         println!("You guessed: {guess}");
     
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
